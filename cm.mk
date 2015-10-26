28cd2898da664c064a8a4abd41067a21958e3b70
Alok Nikhil <nikhil.jan93@ufl.edu>
2015-10-25 21:28:50 -0400

    Revert "Update to ProjectVulpin"
    This reverts commit 50237199243be0678402a409dc46215dc151bd64.

--- cm.mk
+++ cm.mk
@@ -6,10 +6,10 @@ TARGET_SCREEN_WIDTH := 2560
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/vulpin/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/vulpin/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/clark/aosp_clark.mk)
