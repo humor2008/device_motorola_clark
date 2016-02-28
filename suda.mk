544a9ba341cb929beb136b421f9e1905f5c440a3
humor2008 <humorz@gmail.com>
2016-02-28 18:00:37 +0800

    Update suda.mk

--- suda.mk
+++ suda.mk
@@ -5,9 +5,8 @@ TARGET_SCREEN_WIDTH := 1440
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Inherit some common AOKP stuff.
-$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
@@ -18,7 +17,7 @@ $(call inherit-product, device/motorola/clark/aosp_clark.mk)
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aokp_clark
+PRODUCT_NAME := suda_clark
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := XT1570
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
