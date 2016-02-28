5152e5c3022d0af02d8544844760f6fae4916b50
humor2008 <humorz@gmail.com>
2016-02-28 18:05:58 +0800

    Update suda.mk

--- suda.mk
+++ suda.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/clark/aosp_clark.mk)
