35ec3b288fd6e507db717dbd2fde206bf4108540
humor2008 <humorz@gmail.com>
2016-06-09 20:39:58 +0800

    humor modified
    Change-Id: I49143dcfc03f3dca882ed1d79b0d486bdaadcfee

--- tipsy.mk
+++ tipsy.mk
@@ -4,13 +4,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AICP stuff.
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Inherit telephony stuff
 # $(call inherit-product, vendor/validus/config/telephony.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/tipsy/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/clark/device.mk)
@@ -18,10 +18,10 @@ $(call inherit-product, device/motorola/clark/device.mk)
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := validus_clark
+PRODUCT_NAME := tipsy_clark
 PRODUCT_BRAND := Android
 PRODUCT_DEVICE := clark
-PRODUCT_MODEL := XT1575
+PRODUCT_MODEL := XT1570
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
